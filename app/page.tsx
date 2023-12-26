72c7a7e7bca9c7b6c820e1fbd389ae62cd2fa3cf
Tomas <ignaciotomasrodriguezmillan@gmail.com>
2023-12-26 01:42:49 -0300

    change title

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <div className="mb-8">
-        <h1 className="text-4xl font-bold">Bienvenidos</h1>
+        <h1 className="text-4xl font-bold">Tomas dejo de fumar 😬</h1>
       </div>
       <div className="border-8 rounded-2xl border-black p-4">
         <Time />
       </div>
+
     </div>
   );
 }
